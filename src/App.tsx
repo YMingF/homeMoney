33dcde3e989c4e7e38281a30fc5e2291431286aa
Alex <834319313@qq.com>
2022-10-07 21:37:54 +0800

    引入VueRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import { defineComponent, ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
 
 export const App=defineComponent({
     setup(){
-        const count=ref(0);
-        const onClick=()=>{
-            count.value+=1;
-        }
         return ()=><>
-        <div>
-            {count.value}
-        </div>
-        <div onClick={onClick}>
-            +1
-        </div>
+        <router-link to='/about'>about</router-link>
+        <RouterView/>
         </>
     }
 })
