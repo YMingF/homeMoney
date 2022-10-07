5d9bff06dff0e860fca67b14928d53504db64ef2
Alex <834319313@qq.com>
2022-10-07 20:50:35 +0800

    使用jsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'/homeMoney/dist/',
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn:true, // 可用来替换事件的写法
+      mergeProps:true // 把class,style属性自动绑定到子组件的根元素
+    })
+  ]
 })
