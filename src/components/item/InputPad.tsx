aca4690404d8d92aec67da383162af7c52a6f6f4
Alex <834319313@qq.com>
2022-11-05 17:17:54 +0800

    完成数字键盘的输入功能

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -5,22 +5,110 @@ import { DatetimePicker, Popup } from "vant";
 import { time } from "../../shared/time";
 export const InputPad = defineComponent({
   setup(props, context) {
+    const appendText = (n: number | string) => {
+      if (refAmount.value.length >= 10) return;
+      if (
+        refAmount.value.includes(".") &&
+        refAmount.value.split(".")[1].length >= 2
+      ) {
+        return;
+      }
+      const nString = n.toString();
+      switch (nString) {
+        case ".":
+          if (refAmount.value.indexOf(".") !== -1) {
+            return;
+          }
+          break;
+        case "0":
+          if (refAmount.value === "0") {
+            return;
+          }
+          break;
+        default:
+          break;
+      }
+      refAmount.value = (refAmount.value + nString).replace(
+        /^0+(\d+)/,
+        () => RegExp.$1
+      );
+    };
     const buttons = [
-      { text: "1", onClick: () => {} },
-      { text: "2", onClick: () => {} },
-      { text: "3", onClick: () => {} },
-      { text: "4", onClick: () => {} },
-      { text: "5", onClick: () => {} },
-      { text: "6", onClick: () => {} },
-      { text: "7", onClick: () => {} },
-      { text: "8", onClick: () => {} },
-      { text: "9", onClick: () => {} },
-      { text: "清空", onClick: () => {} },
-      { text: "0", onClick: () => {} },
-      { text: ".", onClick: () => {} },
+      {
+        text: "1",
+        onClick: () => {
+          appendText(1);
+        },
+      },
+      {
+        text: "2",
+        onClick: () => {
+          appendText(2);
+        },
+      },
+      {
+        text: "3",
+        onClick: () => {
+          appendText(3);
+        },
+      },
+      {
+        text: "4",
+        onClick: () => {
+          appendText(4);
+        },
+      },
+      {
+        text: "5",
+        onClick: () => {
+          appendText(5);
+        },
+      },
+      {
+        text: "6",
+        onClick: () => {
+          appendText(6);
+        },
+      },
+      {
+        text: "7",
+        onClick: () => {
+          appendText(7);
+        },
+      },
+      {
+        text: "8",
+        onClick: () => {
+          appendText(8);
+        },
+      },
+      {
+        text: "9",
+        onClick: () => {
+          appendText(9);
+        },
+      },
+      {
+        text: "清空",
+        onClick: () => {
+          refAmount.value = "0";
+        },
+      },
+      {
+        text: "0",
+        onClick: () => {
+          appendText(0);
+        },
+      },
+      {
+        text: ".",
+        onClick: () => {
+          appendText(".");
+        },
+      },
       { text: "提交", onClick: () => {} },
     ];
-    const refShowPop = ref(false);
+    const refAmount = ref("0");
     const now = new Date();
     const refDate = ref<Date>(now);
     const refDatePickerVisible = ref(false);
@@ -54,7 +142,7 @@ export const InputPad = defineComponent({
               </Popup>
             </span>
           </span>
-          <div class={s.amount}>200</div>
+          <div class={s.amount}>{refAmount.value}</div>
         </div>
         <div class={s.buttons}>
           {buttons.map((m) => (
