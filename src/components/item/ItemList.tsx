a99d0ec3e4748c441eb376d8d9e409693db95eb3
Alex <834319313@qq.com>
2022-11-14 20:13:48 +0800

    完成自定义时间弹窗的初步效果

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,4 +1,5 @@
-import { defineComponent, reactive, ref } from "vue";
+import { Overlay } from "vant";
+import { defineComponent, reactive, ref, watch } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
 import { Tab, Tabs } from "../../shared/Tabs";
@@ -8,6 +9,7 @@ import { ItemSummary } from "./ItemSummary";
 export const ItemList = defineComponent({
   setup(props, context) {
     const refSelected = ref("本月");
+    const refOverlayVisible = ref(false);
     const time = new Time();
     const customTime = reactive({ start: new Time(), end: new Time() });
     const timeList = [
@@ -18,6 +20,12 @@ export const ItemList = defineComponent({
       },
       { start: time.firstDayOfYear(), end: time.lastDayOfYear() },
     ];
+    watch(
+      () => refSelected.value,
+      () => {
+        refOverlayVisible.value = refSelected.value === "自定义时间";
+      }
+    );
     return () => (
       <>
         <MainLayout>
@@ -25,32 +33,45 @@ export const ItemList = defineComponent({
             title: () => "记账",
             icon: () => <Icon name="menu"></Icon>,
             default: () => (
-              <Tabs v-model:selected={refSelected.value}>
-                <Tab name="本月">
-                  <ItemSummary
-                    startDate={timeList[0].start.format()}
-                    endDate={timeList[0].end.format()}
-                  ></ItemSummary>
-                </Tab>
-                <Tab name="上月">
-                  <ItemSummary
-                    startDate={timeList[1].start.format()}
-                    endDate={timeList[1].end.format()}
-                  ></ItemSummary>
-                </Tab>
-                <Tab name="今年">
-                  <ItemSummary
-                    startDate={timeList[2].start.format()}
-                    endDate={timeList[2].end.format()}
-                  ></ItemSummary>
-                </Tab>
-                <Tab name="自定义时间">
-                  <ItemSummary
-                    startDate={customTime.start.format()}
-                    endDate={customTime.end.format()}
-                  ></ItemSummary>
-                </Tab>
-              </Tabs>
+              <>
+                <Tabs v-model:selected={refSelected.value}>
+                  <Tab name="本月">
+                    <ItemSummary
+                      startDate={timeList[0].start.format()}
+                      endDate={timeList[0].end.format()}
+                    ></ItemSummary>
+                  </Tab>
+                  <Tab name="上月">
+                    <ItemSummary
+                      startDate={timeList[1].start.format()}
+                      endDate={timeList[1].end.format()}
+                    ></ItemSummary>
+                  </Tab>
+                  <Tab name="今年">
+                    <ItemSummary
+                      startDate={timeList[2].start.format()}
+                      endDate={timeList[2].end.format()}
+                    ></ItemSummary>
+                  </Tab>
+                  <Tab name="自定义时间">
+                    <ItemSummary
+                      startDate={customTime.start.format()}
+                      endDate={customTime.end.format()}
+                    ></ItemSummary>
+                  </Tab>
+                </Tabs>
+                <Overlay show={refOverlayVisible.value} class={s.overlay}>
+                  <div class={s.overlay_inner}>
+                    <header>请选择时间</header>
+                    <main>
+                      <form>
+                        <div></div>
+                        <div></div>
+                      </form>
+                    </main>
+                  </div>
+                </Overlay>
+              </>
             ),
           }}
         </MainLayout>
